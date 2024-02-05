5089648c20e5f7017dfa3e0e09c8de336fd07d1d
Johan Sjöblom <sjoblomj88@gmail.com>
2024-02-05 21:02:38 +0100

    Adding support for categories

--- src/sankey.js
+++ src/sankey.js
@@ -38,16 +38,21 @@ function find(nodeById, id) {
 
 function computeLinkBreadths({nodes}) {
   for (const node of nodes) {
-    let y0 = node.y0;
-    let y1 = y0;
-    for (const link of node.sourceLinks) {
-      link.y0 = y0 + link.width / 2;
-      y0 += link.width;
-    }
-    for (const link of node.targetLinks) {
-      link.y1 = y1 + link.width / 2;
-      y1 += link.width;
-    }
+    let getCategories = links => [...new Set(links.map(d => d.category))];
+    let allUniqueCategories = getCategories(node.sourceLinks).concat(getCategories(node.targetLinks))
+    allUniqueCategories.forEach(cat => {
+
+      let y0 = node.y0;
+      let y1 = y0;
+      for (const link of node.sourceLinks.filter(link => link.category === cat)) {
+        link.y0 = y0 + link.width / 2;
+        y0 += link.width;
+      }
+      for (const link of node.targetLinks.filter(link => link.category === cat)) {
+        link.y1 = y1 + link.width / 2;
+        y1 += link.width;
+      }
+    });
   }
 }
 
@@ -146,10 +151,21 @@ export default function Sankey() {
     }
   }
 
+  function computeNodeValue(node) {
+    const sizeForLinks = links => {
+      const categories = [...new Set(links.map(d => d.category))];
+      const sumsForCategories = categories.map(cat => {
+        return {"category": cat, "value": sum(links.filter(link => link.category === cat), value)}
+      });
+      return sumsForCategories.length === 0 ? 0 : max(sumsForCategories, value);
+    }
+    return Math.max(sizeForLinks(node.sourceLinks), sizeForLinks(node.targetLinks));
+  }
+
   function computeNodeValues({nodes}) {
     for (const node of nodes) {
       node.value = node.fixedValue === undefined
-          ? Math.max(sum(node.sourceLinks, value), sum(node.targetLinks, value))
+          ? computeNodeValue(node)
           : node.fixedValue;
     }
   }
